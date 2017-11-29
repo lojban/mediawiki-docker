a67e76022e1a79e784fb286605f13ddb366b5f6d
lojban.org Mediawiki Docker Account <webmaster@lojban.org>
2017-11-28 16:26:02 -0800

    Don't turn everything off when the user exits.  -_-

--- fix_selinux.sh
+++ fix_selinux.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 chcon -t home_bin_t fix_selinux.sh insert_password.sh kill_database.sh kill_web.sh run_database.sh run_web.sh setup.sh build_web.sh
+loginctl show-user sampre_mw | grep Linger=yes || (
+	echo -e "\n\n\nUSER LINGER DISABLED FOR THIS USER.  VERY BAD; MUST FIX.\n\n\n" ;
+	loginctl enable-linger sampre_mw
+)
