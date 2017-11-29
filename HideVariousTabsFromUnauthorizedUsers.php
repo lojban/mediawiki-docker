6264e3d12c51848d08b5a7b703144cccd9b9548e
lojban.org Mediawiki Docker Account <webmaster@lojban.org>
2017-11-28 16:26:17 -0800

    Putting out HideVariousTabsFromUnauthorizedUsers.php  into the repo.

---
+++ HideVariousTabsFromUnauthorizedUsers.php
@@ -0,0 +1,11 @@
+No file /srv/lojban/mediawiki-docker/HideVariousTabsFromUnauthorizedUsers.php found in snapshot jukni-all at revision 137
+Last duplicacy job failed; retrying.
+No file /srv/lojban/mediawiki-docker/HideVariousTabsFromUnauthorizedUsers.php found in snapshot jukni-all at revision 137
+Last duplicacy job failed; retrying.
+No file /srv/lojban/mediawiki-docker/HideVariousTabsFromUnauthorizedUsers.php found in snapshot jukni-all at revision 137
+Last duplicacy job failed; retrying.
+No file /srv/lojban/mediawiki-docker/HideVariousTabsFromUnauthorizedUsers.php found in snapshot jukni-all at revision 137
+Last duplicacy job failed; retrying.
+No file /srv/lojban/mediawiki-docker/HideVariousTabsFromUnauthorizedUsers.php found in snapshot jukni-all at revision 137
+Last duplicacy job failed; retrying.
+All duplicacy attempts failed!
