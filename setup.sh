0485c3e526589e63075e0ce52e1ad3f6f9fdf935
lojban.org Mediawiki Docker Account <webmaster@lojban.org>
2018-01-11 09:11:41 -0800

    Better setup behaviour.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+./fix_selinux.sh
+
 rm -rf ~/.config/systemd/
 mkdir -p ~/.config/systemd/
 ln -s $(pwd)/systemd ~/.config/systemd/user
+
+systemctl --user daemon-reload
 
 systemctl --user enable lojban_mediawiki_db
 systemctl --user start lojban_mediawiki_db
