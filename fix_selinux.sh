e1893a878f8dff84288216d8fba0ec75a55ff774
lojban.org Mediawiki Docker Account <webmaster@lojban.org>
2018-01-11 09:15:28 -0800

    Make the systemd stuff have the right type.

--- fix_selinux.sh
+++ fix_selinux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+chcon -R -t config_home_t systemd/
 chcon -t home_bin_t fix_selinux.sh insert_password.sh kill_database.sh kill_web.sh run_database.sh run_web.sh setup.sh build_web.sh
 loginctl show-user sampre_mw | grep Linger=yes || (
 	echo -e "\n\n\nUSER LINGER DISABLED FOR THIS USER.  VERY BAD; MUST FIX.\n\n\n" ;
