1e9d858e77188aab381073103da5190dfad1f2ae
lojban.org Mediawiki Docker Account <webmaster@lojban.org>
2018-11-18 12:46:20 -0800

    The backup was failing due to lack of TTY in cron space.

--- cron_backup.sh
+++ cron_backup.sh
@@ -8,4 +8,4 @@ cd /home/sampre_mw/mediawiki
 rm data/mysql_backup.sh
 ./insert_password.sh mysql mysql_backup.sh
 sudo docker cp data/mysql_backup.sh lojban_mediawiki_db:/tmp/
-sudo docker exec -it lojban_mediawiki_db /tmp/mysql_backup.sh
+sudo docker exec -t lojban_mediawiki_db /tmp/mysql_backup.sh
