be4a2647aed7cf11feeb6da4e258faf0636c8f8d
lojban.org Mediawiki Docker Account <webmaster@lojban.org>
2020-10-05 23:30:37 -0700

    Added the live/non-cloudflare service.  Also some account name change stuff.

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ systemctl --user enable lojban_mediawiki_db
 systemctl --user start lojban_mediawiki_db
 systemctl --user enable lojban_mediawiki_web
 systemctl --user start lojban_mediawiki_web
+systemctl --user enable lojban_mediawiki_web_live
+systemctl --user start lojban_mediawiki_web_live
 
 # Set up backups
 cat crontab | crontab -
