7bd4c0300e0fdc6ca0bf8f1e981433b9ba45110b
Robin Lee Powell <robinleepowell@gmail.com>
2021-08-07 21:43:20 -0700

    Upgrade to 1.35 and related cleanup.

--- web_options.sh
+++ web_options.sh
@@ -1,6 +1,6 @@
 # This file holds defaults for the command line arguments
 
 # Mediawiki version number
-DEFAULT_MW_VERSION=1.34
+DEFAULT_MW_VERSION=1.35
 # our sub-version number; used to force rebuilds
 DEFAULT_ITERATION=1
